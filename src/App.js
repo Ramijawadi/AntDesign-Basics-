d0e6b7f001f7d65c125fe28d8751bb08940f0880
rami jawadi <ramijawadi104@gmail.com>
2023-10-12 09:50:10 +0100

    FEATURE : add configProvider to costumise the theme

--- src/App.js
+++ src/App.js
@@ -15,7 +15,29 @@ token : {
         <Space direction='vertical' size={12}>
      <Button type='primary'>Theme Button</Button>
      <Radio>Radio</Radio>
+<ConfigProvider theme={{
+
+  inherit: false 
+}}>
+
+     <Radio>Radio without theme</Radio>
+     </ConfigProvider>
      <Checkbox>Checkbox</Checkbox>
+     <Input placeholder='write here ...' />
+     <Select placeholder='select' options={[
+{label:"option1", 
+value:'options1'},
+{label:"option2", 
+value:'options2'},
+{label:"option3", 
+value:'options3'},
+
+
+     ]}  ></Select>
+
+
+<Table columns={[{ title:"Column",  dataIndex:'col'}]} dataSource={[{col: "value1"}]} />
+
         </Space>     
 
      </ConfigProvider>
