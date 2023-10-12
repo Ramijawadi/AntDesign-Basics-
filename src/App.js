0ed65d60853125af7eec1a2d37fb9f67d86fda23
rami jawadi <ramijawadi104@gmail.com>
2023-10-12 09:40:06 +0100

    FIX : setup project + installing Antd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Button , Input ,Select , Radio , Checkbox , Table , ConfigProvider, Space } from 'antd'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ConfigProvider  theme={{
+
+token : {
+
+  colorPrimary:"green",
+},
+
+      }}>
+        <Space direction='vertical' size={12}>
+     <Button type='primary'>Theme Button</Button>
+     <Radio>Radio</Radio>
+     <Checkbox>Checkbox</Checkbox>
+        </Space>     
+
+     </ConfigProvider>
     </div>
   );
 }
